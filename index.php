9d14300e07142ab926ed92bcd4c7334f33dec914
Armando Castillo <armando.castillo@sundevs.com>
2018-10-24 17:13:09 -0500

    Add chrome flag parameter

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ Log::print("Redirecting unauthenticated traffic by MAC: $client_mac", "message",
 
 // Get redirected URL
 $base_url = ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 'https' : 'http' ) . '://' .  $_SERVER['HTTP_HOST'];
-$url = $base_url . $_SERVER["REQUEST_URI"];
+$url = $base_url . $_SERVER["REQUEST_URI"] . '&chrome=true';
 
 $hidden_fields_array = array(
     'token' => $token,
