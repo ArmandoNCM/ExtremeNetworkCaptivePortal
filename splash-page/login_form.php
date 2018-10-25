a45b7c61a4beb4750874f23ca87d04ce5c6df33c
Armando Castillo <armando.castillo@sundevs.com>
2018-10-25 17:50:04 -0500

    Fix styling issue

--- splash-page/login_form.php
+++ splash-page/login_form.php
@@ -12,7 +12,7 @@ $html_form_process_url = '/ExtremeNetworksCaptivePortal/splash-page/form_process
 <body>
 	<div id="header">
 		<img src="/ExtremeNetworksCaptivePortal/splash-page/assets/images/logo-audi.png">
-		<p>Bienvenido a la nueva era de Audi.</p>
+		<p id="header-message">Bienvenido a la nueva era de Audi.</p>
 	</div>
 	<div id="information">
 		<span>Red Wi-Fi gratuita</span>
