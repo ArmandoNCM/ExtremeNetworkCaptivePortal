972017465dbbaf611a53a3e675649b3d029937cf
Armando Castillo <armando.castillo@sundevs.com>
2018-10-25 15:33:19 -0500

    Reduce session time to 60 seconds

--- splash-page/grant_access.php
+++ splash-page/grant_access.php
@@ -9,7 +9,7 @@ $shared_secret = constant('SHARED_SECRET');
 $useHttps = FALSE;
 $assigned_role = NULL;
 $destination = 'http://kenshin.sundevs.cloud/ExtremeNetworksCaptivePortal/qrCodes/' . $qrCodeName . '.png';
-$session_time = 3600;
+$session_time = 60;
 
 Log::print('Destination URL: ' . $destination, 'debug', __FILE__, __LINE__);
 $unsignedUrl = SimpleAWS::makeUnsignedUrl($controller_ip, $controller_port, $useHttps, $token, $person_email, $wlan_identifier, $assigned_role, $destination, $session_time);
