7405b08876858657f031a707df0635e5f2251980
Armando Castillo <armando.castillo@sundevs.com>
2018-12-06 11:51:25 -0500

    Added logging statement to get signed url in authentication attempt Remove tarjeta joven redirection

--- splash-page/grant_access.php
+++ splash-page/grant_access.php
@@ -17,7 +17,7 @@ $service = 'ecp';
 $signature_expiration_time = 30;
 
 $signedUrl = SimpleAWS::createPresignedUrl($unsignedUrl, $identity, $shared_secret, $region, $service, $signature_expiration_time);
-
+Log::print("Attempting to Authenticate with Signed URL:\n\n\n--->   $signedUrl   <---", "info", __FILE__, __LINE__);
 header('Location: '.$signedUrl);
 
 exit();
