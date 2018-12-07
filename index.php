759081111a85f8843d4546735f2e9e6d749e42f0
Armando Castillo <armando.castillo@sundevs.com>
2018-12-07 08:31:35 -0500

    Re-enable bypass login if already registered

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once(dirname(__FILE__).'/class/MacFormatter.php');
 $browserData = get_browser(NULL, TRUE);
 $browser = $browserData['browser'];
 $platform = $browserData['platform'];
+$open_external_browser = ($platform === 'Android');
 Log::print("Browser and Platform: $browser & $platform", "info", __FILE__, __LINE__);
 
 // Get authorization data
@@ -30,21 +31,19 @@ $hidden_fields_array = array(
     'controller_ip' => $controller_ip,
     'controller_port' => $controller_port,
     'wlan_identifier' => $wlan_identifier,
-    'open_external_browser' => ($platform === 'Android')
+    'open_external_browser' => $open_external_browser
 );
 
-// $apiUrl = constant('API_URL') . '/exhibition-forms/expo-students/exists/' . $client_mac;
-// $apiResponse = Tool::perform_http_request('GET', $apiUrl);
+$apiUrl = constant('API_URL') . '/exhibition-forms/expo-cund/exists/' . $client_mac;
+$apiResponse = Tool::perform_http_request('GET', $apiUrl);
 
 $alreadyRegistered = (isset($apiResponse) && array_key_exists('response_code', $apiResponse) && $apiResponse['response_code'] == 204);
 
-// if (isset($alreadyRegistered) && $alreadyRegistered) {
-//     require_once(dirname(__FILE__) . '/splash-page/grant_access.php');
-//     header('Location: http://www.cundinamarca.gov.co');
-//     exit();
-// } else {
+if (isset($alreadyRegistered) && $alreadyRegistered) {
+    require_once(dirname(__FILE__) . '/splash-page/grant_access.php');
+} else {
     require_once(dirname(__FILE__).'/splash-page/login_form.php');
-// }
+}
 
 
 ?>
